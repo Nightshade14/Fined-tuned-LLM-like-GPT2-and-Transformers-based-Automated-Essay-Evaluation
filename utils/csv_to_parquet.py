16403ad826202e304d68bd1d6f72adb6011217c5
Nightshade14 <satyamchatrola14@gmail.com>
2024-06-27 03:24:16 -0400

    feat: Made the conversion file more robust
    Also remove the .csv file getting tracked and only track .parquet file

--- utils/csv_to_parquet.py
+++ utils/csv_to_parquet.py
@@ -1,9 +1,20 @@
-import sys
+import argparse
+import traceback
 import pandas as pd
-filePath = sys.argv[1]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--src_file", help="Source file path (csv)")
+parser.add_argument("-d", "--dest_file", help="Destination file path (parquet)")
+args = parser.parse_args()
+
+if not args.dest_file:
+	args.dest_file = args.src_file[:-3] + "parquet"
+
 try:
-	df = pd.read_csv(filePath+"train.csv")
-	df.to_parquet(filePath+"train.parquet", index=False)
+	df = pd.read_csv(args.src_file)
+	df.to_parquet(args.dest_file, index=False)
 	print("File converted successfully")
 except:
-	print("error")
+	traceback.print_exc()
+finally:
+	print("Program execution completed.")
