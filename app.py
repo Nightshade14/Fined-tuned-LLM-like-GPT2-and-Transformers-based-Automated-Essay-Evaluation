fb5d5f04ea2de4f3cac56b3bcfb5e16c10fc4595
Nightshade14 <satyamchatrola14@gmail.com>
2024-06-30 23:33:43 -0400

    feat: Make the server more robust
    Add the error-handling ability

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from data_schema.Data import Data
 from transformers import AutoModelForSequenceClassification
+import traceback
 
 app = FastAPI()
 
@@ -10,9 +11,13 @@ def get_home_page():
 
 @app.post("/evaluate")
 def evaluate_essay(data: Data):
-	data_dict = data.model_dump()
-	NUM_CLASSES = 6
-	essay = data_dict.get("essay")
-	model_id = data_dict.get("model")
-	model = AutoModelForSequenceClassification.from_pretrained('./model/my_model', num_labels=NUM_CLASSES)
-	return type(data_dict)
+	try:
+		data_dict = data.model_dump()
+		NUM_CLASSES = 6
+		essay = data_dict.get("essay")
+		model_id = data_dict.get("model")
+		model = AutoModelForSequenceClassification.from_pretrained('./model/my_model1', num_labels=NUM_CLASSES)
+		return "success"
+	except:
+		traceback.print_exc()
+		return "error"
